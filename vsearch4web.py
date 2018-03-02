d6b7d7e961efae1927ace808251f284c4ac826b6
maxx <max.duhota@gmail.com>
2018-03-02 19:35:56 +0200

    final 11 chapter

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, escape, session
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from vsearch import search4letters
 from checker import check_logged_in
 
@@ -41,7 +41,10 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except Exception as err:
+        print('***** Logging failed with this error:', str(err))
     return render_template('results.html',
                            the_title=title,
                            the_phrase = phrase,
@@ -58,16 +61,25 @@ def entry_page() -> 'html':
 @check_logged_in
 def view_the_log() -> 'html':
     """Выводит содержимое файла журнала в виде HTML-таблицы"""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        sql = """select phrase, letters, ip, browser_string, results from log"""
-        cursor.execute(sql)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                           the_title='View Log',
-                           the_row_titles=titles,
-                           the_data=contents,)
-
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            sql = """select phrase, letters, ip, browser_string, results from log"""
+            cursor.execute(sql)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                               the_title='View Log',
+                               the_row_titles=titles,
+                               the_data=contents,)
+    except ConnectionError as err:
+        print('Is your database switced on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
 
